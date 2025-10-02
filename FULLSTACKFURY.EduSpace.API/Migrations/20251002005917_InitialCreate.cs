20a2b9c4f608f3be71577c7e09da04d4d0dc6bab
Andres Torres <andresalbertotorresgarcia@gmail.com>
2025-10-01 20:19:09 -0500

    feat: add initial database migration with schema for accounts, classrooms, meetings, and related entities

---
+++ FULLSTACKFURY.EduSpace.API/Migrations/20251002005917_InitialCreate.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace FULLSTACKFURY.EduSpace.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "account_ids",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("p_k_account_ids", x => x.id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "accounts",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    username = table.Column<string>(type: "longtext", nullable: false),
+                    password_hash = table.Column<string>(type: "longtext", nullable: false),
+                    role = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("p_k_accounts", x => x.id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "classrooms",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    name = table.Column<string>(type: "longtext", nullable: false),
+                    description = table.Column<string>(type: "longtext", nullable: false),
+                    teacher_id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("p_k_classrooms", x => x.id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "meetings",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    title = table.Column<string>(type: "longtext", nullable: false),
+                    description = table.Column<string>(type: "longtext", nullable: false),
+                    date = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    start_time = table.Column<TimeSpan>(type: "time(6)", nullable: false),
+                    end_time = table.Column<TimeSpan>(type: "time(6)", nullable: false),
+                    administrator_id = table.Column<int>(type: "int", nullable: false),
+                    classroom_id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("p_k_meetings", x => x.id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "reservations",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    title = table.Column<string>(type: "longtext", nullable: false),
+                    start = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    end = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    area_id = table.Column<int>(type: "int", nullable: false),
+                    teacher_id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("p_k_reservations", x => x.id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "shared_areas",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    name = table.Column<string>(type: "longtext", nullable: false),
+                    capacity = table.Column<int>(type: "int", nullable: false),
+                    description = table.Column<string>(type: "longtext", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("p_k_shared_areas", x => x.id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "admin_profiles",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    first_name = table.Column<string>(type: "longtext", nullable: false),
+                    last_name = table.Column<string>(type: "longtext", nullable: false),
+                    email = table.Column<string>(type: "longtext", nullable: false),
+                    dni = table.Column<string>(type: "longtext", nullable: false),
+                    address = table.Column<string>(type: "longtext", nullable: false),
+                    phone = table.Column<string>(type: "longtext", nullable: false),
+                    account_id_id = table.Column<int>(type: "int", nullable: false),
+                    created_at = table.Column<DateTimeOffset>(type: "datetime", nullable: true),
+                    updated_at = table.Column<DateTimeOffset>(type: "datetime", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("p_k_admin_profiles", x => x.id);
+                    table.ForeignKey(
+                        name: "f_k_admin_profiles__account_id_account_id_id",
+                        column: x => x.account_id_id,
+                        principalTable: "account_ids",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "teacher_profiles",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    administrator_id = table.Column<int>(type: "int", nullable: false),
+                    first_name = table.Column<string>(type: "longtext", nullable: false),
+                    last_name = table.Column<string>(type: "longtext", nullable: false),
+                    email = table.Column<string>(type: "longtext", nullable: false),
+                    dni = table.Column<string>(type: "longtext", nullable: false),
+                    address = table.Column<string>(type: "longtext", nullable: false),
+                    phone = table.Column<string>(type: "longtext", nullable: false),
+                    account_id_id = table.Column<int>(type: "int", nullable: false),
+                    created_at = table.Column<DateTimeOffset>(type: "datetime", nullable: true),
+                    updated_at = table.Column<DateTimeOffset>(type: "datetime", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("p_k_teacher_profiles", x => x.id);
+                    table.ForeignKey(
+                        name: "f_k_teacher_profiles__account_id_account_id_id",
+                        column: x => x.account_id_id,
+                        principalTable: "account_ids",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "resources",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    name = table.Column<string>(type: "longtext", nullable: false),
+                    kind_of_resource = table.Column<string>(type: "longtext", nullable: false),
+                    classroom_id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("p_k_resources", x => x.id);
+                    table.ForeignKey(
+                        name: "f_k_resources_classrooms_classroom_id",
+                        column: x => x.classroom_id,
+                        principalTable: "classrooms",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "meeting_sessions",
+                columns: table => new
+                {
+                    meeting_id = table.Column<int>(type: "int", nullable: false),
+                    teacher_id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("p_k_meeting_sessions", x => new { x.meeting_id, x.teacher_id });
+                    table.ForeignKey(
+                        name: "f_k_meeting_sessions_meetings_meeting_id",
+                        column: x => x.meeting_id,
+                        principalTable: "meetings",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "f_k_meeting_sessions_teacher_profiles_teacher_id",
+                        column: x => x.teacher_id,
+                        principalTable: "teacher_profiles",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "i_x_admin_profiles_account_id_id",
+                table: "admin_profiles",
+                column: "account_id_id");
+
+            migrationBuilder.CreateIndex(
+                name: "i_x_meeting_sessions_teacher_id",
+                table: "meeting_sessions",
+                column: "teacher_id");
+
+            migrationBuilder.CreateIndex(
+                name: "i_x_resources_classroom_id",
+                table: "resources",
+                column: "classroom_id");
+
+            migrationBuilder.CreateIndex(
+                name: "i_x_teacher_profiles_account_id_id",
+                table: "teacher_profiles",
+                column: "account_id_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "accounts");
+
+            migrationBuilder.DropTable(
+                name: "admin_profiles");
+
+            migrationBuilder.DropTable(
+                name: "meeting_sessions");
+
+            migrationBuilder.DropTable(
+                name: "reservations");
+
+            migrationBuilder.DropTable(
+                name: "resources");
+
+            migrationBuilder.DropTable(
+                name: "shared_areas");
+
+            migrationBuilder.DropTable(
+                name: "meetings");
+
+            migrationBuilder.DropTable(
+                name: "teacher_profiles");
+
+            migrationBuilder.DropTable(
+                name: "classrooms");
+
+            migrationBuilder.DropTable(
+                name: "account_ids");
+        }
+    }
+}
